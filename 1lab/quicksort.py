fd80e85328d85ebe0eaf5bcf54901393482a9c85
Jesper <j.soberg@stud.uis.no>
2024-01-18 11:22:31 +0100

    quicksort-skritteller

--- 1lab/quicksort.py
+++ 1lab/quicksort.py
@@ -1,17 +1,23 @@
-def quicksort(liste, start, slutt):
+def quicksort(liste, start, slutt, steg=0):
     if start < slutt:
-        midt = partition(liste, start, slutt)
-        quicksort(liste, start, midt-1)
-        quicksort(liste, midt+1, slutt)
-    return liste
+        midt, partition_steg = partition(liste, start, slutt)
+        steg += partition_steg
+        quicksort(liste, start, midt-1, steg)
+        quicksort(liste, midt+1, slutt, steg)
+    return steg
 
 
 def partition(liste, start, slutt):
+    steg = 0
     pivot = liste[slutt]
     i = start-1
+    steg += 2
     for j in range(start, slutt):
         if liste[j] <= pivot:
             i += 1
             liste[i], liste[j] = liste[j], liste[i]
+            steg += 2
+        steg += 1
     liste[i+1], liste[slutt] = liste[slutt], liste[i+1]
-    return i+1
+    steg += 1
+    return i+1, steg
