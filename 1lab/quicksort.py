781e0675b9918d611e30287871cc0d780bed5a3f
Jesper <j.soberg@stud.uis.no>
2024-01-18 11:09:24 +0100

    ny quicksort

--- 1lab/quicksort.py
+++ 1lab/quicksort.py
@@ -1,35 +1,17 @@
-def quicksort(list):
-    if len(list) > 1:
-        pivot = list[0]
+def quicksort(liste, start, slutt):
+    if start < slutt:
+        midt = partition(liste, start, slutt)
+        quicksort(liste, start, midt-1)
+        quicksort(liste, midt+1, slutt)
+    return liste
 
-        front = 1
-        back = len(list)-1
 
-        left = []
-        right = []
-
-        while front < back:
-            while list[front] <= pivot and front < back:
-                left.append(list[front])
-                front += 1
-            while list[back] > pivot and back > front:
-                right.append(list[back])
-                back -= 1
-            if front < back:
-                left.append(list[back])
-                right.append(list[front])
-                front += 1
-                back -= 1
-        
-        if list[back] <= pivot:
-            left.append(list[back])
-        else:
-            right.append(list[back])
-
-        left.append(pivot)
-        
-        left = quicksort(left)
-        right = quicksort(right)
-        
-        return left + right
-    return list
+def partition(liste, start, slutt):
+    pivot = liste[slutt]
+    i = start-1
+    for j in range(start, slutt):
+        if liste[j] <= pivot:
+            i += 1
+            liste[i], liste[j] = liste[j], liste[i]
+    liste[i+1], liste[slutt] = liste[slutt], liste[i+1]
+    return i+1
