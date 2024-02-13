491aaed2bd549be9501fd72eb2cd93afd1fa03d7
Creeger <magnus.tysdal@gmail.com>
2024-02-13 12:55:55 +0100

    .\FractionalKnapSack.py ferdig

--- 2lab/Knapsack/FractionalKnapSack.py
+++ 2lab/Knapsack/FractionalKnapSack.py
@@ -4,5 +4,19 @@ def solveFractionalKnapSack(weights: list, prices: list, capacity: int):
 		pricePerKg = prices[i]/weights[i]
 		pricesPerKg.append((i, pricePerKg))
 
-	pricesPerKg.sort(key=lambda tuple: tuple[1])
+	pricesPerKg.sort(key=lambda tuple: tuple[1], reverse=True)
 	print(pricesPerKg)
+
+	solution = []
+	totalWeight = 0
+	for i, pricePerKg in pricesPerKg:
+		if totalWeight + weights[i] <= capacity:
+			totalWeight += weights[i]
+			solution.append((i, weights[i]))
+		elif totalWeight == capacity:
+			break
+		else:
+			solution.append((i, capacity-totalWeight))
+			break
+
+	return solution
