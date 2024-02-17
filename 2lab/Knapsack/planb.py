77d9f525a9d7a57cb1043947e1fdd169be84e3a0
Jesper <j.soberg@stud.uis.no>
2024-02-17 19:08:38 +0100

    top-down binary knapsack

---
+++ 2lab/Knapsack/planb.py
@@ -0,0 +1,44 @@
+def binaryKnapsack(weights, prices, capacity):
+    indeces = [i for i in range(len(weights))]
+    solutions = {}
+    return solvebinaryKnapsack(indeces, weights, prices, capacity, solutions)
+
+
+def solvebinaryKnapsack(indeces, weights, prices, capacity, solutions):
+
+    problem = "".join([str(x) for x in indeces]) + f",{capacity}"
+    if problem in solutions.keys():
+        return solutions[problem]
+
+    if len(indeces) == 1:
+        index = indeces[0]
+        if(weights[index] <= capacity):
+            solution = (set(indeces), prices[index])
+        else:
+            solution = (set(), 0)
+
+        solutions[problem] = solution
+        return solution
+
+
+    best_solution = (set(), 0)
+    for i in indeces:
+        temp_indeces = indeces[:]
+        temp_indeces.remove(i)
+        temp_capacity = capacity - weights[i]
+
+        (solution_indeces, solution_price) = solvebinaryKnapsack(temp_indeces, weights, prices, temp_capacity, solutions)
+        solution_indeces = solution_indeces.copy()
+        solution_indeces.add(i)
+        solution_price += prices[i]
+        solution = (solution_indeces, solution_price)
+
+        if solution[1] > best_solution[1]:
+            best_solution = solution
+
+    solutions[problem] = best_solution
+    return best_solution
+
+
+if __name__ == "__main__":
+    print(binaryKnapsack([10, 20, 30], [60, 100, 120], 50))
